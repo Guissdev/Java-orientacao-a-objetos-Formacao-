618dd4be7746351a5979f83b305ace806a645c86
Guilherme Santos <guiss.devs@gmail.com>
2023-04-16 15:16:16 -0300

    Object referencia

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
@@ -1,35 +1,46 @@
 package br.com.bytebank.banco.test;
 
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
 		
 		//int[] idades = new int [5];
-		ContaCorrente[] contas = new ContaCorrente[5];
+		Object[] referencias = new Object[5];
 	
 		ContaCorrente cc1 = new ContaCorrente(22, 11);
-		ContaCorrente cc2 = new ContaCorrente(33, 22);
+		ContaPoupanca cc2 = new ContaPoupanca(33, 22);
 		ContaCorrente cc3 = new ContaCorrente(44, 33);
 		ContaCorrente cc4 = new ContaCorrente(55, 44);
 		ContaCorrente cc5 = new ContaCorrente(66, 55);
 		
-		contas[0] = cc1;
-		contas[1] = cc2;
-		contas[2] = cc3;
-		contas[3] = cc4;
-		contas[4] = cc5;
+		referencias[0] = cc1;
+		referencias[1] = cc2;
+		referencias[2] = cc3;
+		referencias[3] = cc4;
+		referencias[4] = cc5;
 		
-		for(int i = 0; i < contas.length; i++) {
-			System.out.println( contas[i]);
+		for(int i = 0; i < referencias.length; i++) {
+			System.out.println( referencias[i]);
 	}
+		Cliente cliente = new Cliente();
+		referencias[2] = cliente;
+		
+		
 		//maneiras de ferenciar
 		
-		System.out.println(contas[1].getNumero());
+		//System.out.println(cc2.getNumero());	
 		
-		ContaCorrente ref = contas[1];
+//		Object referenciaGenerica = contas[1];
+//		
+//		System.out.println(referenciaGenerica.getNumero());
+		
+		ContaPoupanca ref = (ContaPoupanca)referencias[1]; //Type cast
 		System.out.println(cc2.getNumero());
 		System.out.println(ref.getNumero());
-}	
+	}	
 }
